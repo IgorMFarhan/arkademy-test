ab597e73467bfba595da7db4b22d26ef7c578044
IgorMFarhan <igormfarhan2@gmail.com>
2019-11-23 13:48:56 +0700

    Selesai soal no.1

---
+++ 1.py
@@ -0,0 +1,44 @@
+import json
+
+def biodata(name,age):
+    myBiodata =  {
+        'name' : name,
+        'age' : age,
+        'address' : 'Vila Mutiara Lido C3/12, Cigombong, Bogor',
+        'hobbies' : ['Badminton','Swimming','Cooking','Reading'],
+        'is_married' : False,
+        'list_school' : [{
+            'elementarySchool' : 'SDN Cigombong 1',
+            'year_in' : 2002,
+            'year_out' : 2008,
+            'major' : None},
+            {
+            'middleSchool' : 'SMPN 1 Cigombong',
+            'year_in' : 2008,
+            'year_out' : 2011,
+            'major' : None},
+            {
+            'highSchool' : 'SMAN 1 Cigombong',
+            'year_in' : 2011,
+            'year_out' : 2014,
+            'major' : 'Natural Science'},
+            {  
+            'university' : 'Universitas Sebelas Maret',
+            'year_in' : 2014,
+            'year_out' : 2018,
+            'major' : 'Electrical Engineering'}],
+        
+        'skills' : [{
+            'skill_name' : 'Web Programming',
+            'level' : 'Advanced'},
+            {
+            'skill_name' : 'Microcontroller programming',
+            'level' : 'Expert'}],
+        
+        'interested_in_coding' : True
+    }
+
+    return json.dumps(myBiodata)
+
+
+print(biodata('Igor',23))
